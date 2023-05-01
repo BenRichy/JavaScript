9796ac03c47204b37f1d097c58dde5f50100fe5e
Ben Richmond (REE) <ben.richmond@ricardo.com>
2023-05-01 13:25:20 +0100

    added GeneralFundamentals2

---
+++ Fundamentals/Fundamentals2/GeneralFundamental2.js
@@ -0,0 +1,12 @@
+// non-challenges from fundamental 2
+
+//strict mode makes it harder to introduce errors into code
+//forbids you to do certain things, and gives you errors
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// logic to change drivers license to true
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
